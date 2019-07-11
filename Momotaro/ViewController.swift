d823be7bb0aa48c3a1cd796eeb610ec54b64179a
yukimasa ikeda <ykms123456789@gmail.com>
2019-07-11 21:43:54 +0900

    Create a program to move the first page picture

--- Momotaro/ViewController.swift
+++ Momotaro/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 【1】画像データをインスタンス化
+        let imageA: UIImage! = UIImage(named: "Image1-A")
+        let imageB: UIImage! = UIImage(named: "Image1-B")
+        // 【2】コマ送りに使う画像データの配列をセット
+        Image.animationImages = [imageA, imageB]
+        // 【3】コマ送りの間隔を設定
+        Image.animationDuration = 1
+        // 【4】コマ送りのアニメーションを開始
+        Image.startAnimating()
     }
 
 
